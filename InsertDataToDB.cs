a2db816afbebcaec12b5e16c2f6a5da19e49bb5c
Mateusz Stacel <42943907+mateuszstacel@users.noreply.github.com>
2019-03-28 11:29:16 +0000

    Update InsertDataToDB.cs

--- InsertDataToDB.cs
+++ InsertDataToDB.cs
@@ -4,7 +4,13 @@
             
             try
             {
+                    
                 string connectionString = "Data Source=000.000.00.00; initial catalog=Field_Interviewer_Maps; user id=*****; password=*****;";
+               //better Practice is this option 2 Need to install via nugets System.Configuration
+               //Conection in App.config
+                    // <add name="InterviewerEntities" connectionString="Data Source=*****; initial catalog=Field_Interviewer_Maps; user id=sa; password=***;" providerName="System.Data.SqlClient" />
+               string connectionString = ConfigurationManager.ConnectionStrings["InterviewerEntities"].ToString();
+                    
                 SqlConnection conn = new SqlConnection(connectionString);
                 conn.Open();
                 SqlCommand cmd = new SqlCommand("UPDATE interviewers_details SET Lat =" + words[0] + ", Lng =" + words[1] + "WHERE ID =" + ID.ToString() +";", conn);
