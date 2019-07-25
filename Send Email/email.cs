a630a5ce33ea97301943061f0fa97f4ce6670341
Mateusz Stacel <42943907+mateuszstacel@users.noreply.github.com>
2019-07-25 09:47:01 +0100

    Update email.cs

--- Send Email/email.cs
+++ Send Email/email.cs
@@ -10,3 +10,27 @@ MailMessage mail = new MailMessage("from", "to...");
                 mail.Subject = "this is a test email.";
                 mail.Body = "this is my test email body";
                 client.Send(mail);
+
+
+
+
+
+
+        private static void SendEmail(string subject, string email)
+        {
+            MailMessage mail = new MailMessage("alert@speechanalytics", "mateusz.stacel@bmgresearch.co.uk");
+            SmtpClient client = new SmtpClient("172.16.0.43");
+            client.Port = 25;
+            client.UseDefaultCredentials = false;
+            client.EnableSsl = false;
+            client.Credentials = new NetworkCredential("", "");
+            //mail.To.Add("add more peoples...");
+            mail.Subject = subject;
+            mail.IsBodyHtml = true;
+
+
+            mail.Body = email;
+
+
+            client.Send(mail);
+        }
